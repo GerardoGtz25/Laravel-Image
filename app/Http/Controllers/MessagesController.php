ea16c2984b90f1fb09e06ca884d4b6d489a7b7f4
Gerardo <ge.gut5@gmail.com>
2019-02-11 12:11:15 -0600

    Primer commit

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use App\Http\Requests\CreateMessageRequest;
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller
+{
+    public function show (Message $message) {
+
+        // $messages = Message::find($id);
+
+        return view('messages.show', [
+            'message' => $message,
+        ]);
+
+    }
+
+    public function create(CreateMessageRequest $request) {
+
+        $message = Message::create([
+            'content' => $request->input('message'),
+            'image' => 'http://lorempixel.com/600/338?'.mt_rand(0, 1000),
+            'user_id' => 1
+            ]);
+
+            return redirect('/messages/'.$message->id);
+        
+    }
+
+}
